28529b71985a2de2113a9d6aa867e93f7935fff7
Bizara <z50205@yahoo.com.tw>
2022-05-04 00:29:57 +0800

    增加懸臂梁端設定

--- Program.cs
+++ Program.cs
@@ -71,20 +71,21 @@ namespace HelloWorld
             double breadth = Convert.ToDouble(s[2].Substring(11, 5).Replace(" ", ""));
             double depth = Convert.ToDouble(s[2].Substring(16, 5).Replace(" ", ""));
             s_new = change_third_row(s, breadth, depth);
+            s_new =change_cantilever(s_new);
             return s_new;
         }
         public static string[] change_third_row(string[] s, double B, double D)
         {
             string[] s_new = new string[12];
-            s_new[0]=s[0];
-            s_new[1]=s[1];
-            s_new[2]=s[2];
-            s_new[3]=s[3];
-            s_new[10]=s[10];
-            s_new[11]=s[11];
+            s_new[0] = s[0];
+            s_new[1] = s[1];
+            s_new[2] = s[2];
+            s_new[3] = s[3];
+            s_new[10] = s[10];
+            s_new[11] = s[11];
             int total_bar = 0;
             int max_bar = 0;
-            string [] bar_new;
+            string[] bar_new;
             // Console.WriteLine(s.Substring(20, 2).Replace(" ", ""));
             for (int i = 4; i <= 9; i++)
             {
@@ -97,14 +98,16 @@ namespace HelloWorld
                     //改變排列主邏輯
                     bar_new = change_bar(total_bar, max_bar);
                     //輸出並替代該端鋼筋
-                    s_new[i]=bar_new[0]+s[i].Substring(2, 8)+bar_new[1]+s[i].Substring(12, 8)+bar_new[2]+s[i].Substring(22, 58);
+                    s_new[i] = bar_new[0] + s[i].Substring(2, 8) + bar_new[1] + s[i].Substring(12, 8) + bar_new[2] + s[i].Substring(22, 58);
+                    Console.WriteLine( s[0].Substring(0, 17) + "\n原鋼筋排列" + s[i].Substring(0, 2) + " " + s[i].Substring(10, 2) + " " + s[i].Substring(20, 2) + " ");
+                    Console.WriteLine("總鋼筋根數" + total_bar + "\n" + "調整後鋼筋排列" + bar_new[0] + " " + bar_new[1] + " " + bar_new[2]);
                     //s_new[i]=bar_new[0]+s[i].Substring(2, 9)+bar_new[1]+s[i].Substring(12, 19)+bar_new[2]+s[i].Substring(22, 80);
                 }
-                else 
-                s_new[i]=s[i];
+                else
+                    s_new[i] = s[i];
 
             }
-            Console.WriteLine(total_bar);
+            //Console.WriteLine(total_bar);
 
             return s_new;
         }
@@ -149,11 +152,36 @@ namespace HelloWorld
                 bar[1] = residual_bar;
             residual_bar -= bar[1];
             bar[2] = residual_bar;
-            for (int i=0;i<=2;i++)
-               bar_str[i]=bar[i].ToString().PadLeft(2, ' ');
+            for (int i = 0; i <= 2; i++)
+                bar_str[i] = bar[i].ToString().PadLeft(2, ' ');
             return bar_str;
         }
+        public static string[] change_cantilever(string[] s)
+        {
+            string[] s_new = new string[12];
+            string[] s_tempt = s[0].Split(')');
+            if (s_tempt[1].Substring(0, 1) == "C" | s_tempt[1].Substring(0, 1) == "c")
+            {
+                char keyin = '7';
+                //while防呆輸入，設定初始值必進入迴圈
+                while (!(keyin == '1'||keyin == '2'||keyin == '3'))
+                {
+                    Console.WriteLine(s[0].Substring(0,17)+"\n本梁前綴為懸臂梁，修改左端：1；修改右端：2；不修改：3\n");
+                    keyin = Console.ReadKey(true).KeyChar;
+                }
+                if(keyin=='1')
+                {
+                    s[2]=s[2].Substring(0,37)+"15. - 0"+s[2].Substring(44,36);
+                }
+                if(keyin=='2')
+                {
+                    s[3]=s[3].Substring(0,37)+"15. - 0"+s[3].Substring(44,36);
+                }
+            }
+            return s;
+        }
     }
+
     // class beam
     // {
     //     public string beam_name;
